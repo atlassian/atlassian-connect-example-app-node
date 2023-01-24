37d406f3668a351facd64534527f51722f890f73
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-01-24 14:26:46 +1100

    - code cleanup

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,16 +3,13 @@
  * it changes everytime you run a new tunnel
  * This URL is needed in the connect-app-descriptor json
  */
-const baseUrl = async (): Promise<string> => fetch('http://tunnel:4040/api/tunnels')
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`ngrok tunnel error: ${response}`);
-        }
-
-        return response.json();
-    })
-    .then((data) => {
-        return data.tunnels[0].public_url;
-    });
+const baseUrl = async (): Promise<string> => {
+    const response = await fetch('http://tunnel:4040/api/tunnels');
+    if (!response.ok) {
+        throw new Error(`ngrok tunnel error: ${response}`);
+    }
+    const data = await response.json();
+    return data.tunnels[0].public_url;
+}
 
 export default baseUrl;
