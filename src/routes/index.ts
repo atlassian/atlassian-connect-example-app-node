0c675baaed0cc11b12610c89a1896023d59983a7
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-01 12:09:26 +1100

    - Link to atlassian connect added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,16 +7,17 @@ const routes = Router();
 routes.get('/', (_req, res) => {
     res.render('index.mst', {
         index: 'Index Page',
-        body: 'This is the body'
+        body: 'You in the home page!'
     });
 });
 
 routes.get('/config', async (_req, res) => {
-    const props = {
-        baseUrl: await baseUrl(),
-    }
+    const props = { baseUrl: await baseUrl() };
 
-    res.json(connectAppDescriptor(props));
+    res.render('config.mst', {
+        index: 'Connect app descriptor',
+        config: JSON.stringify(connectAppDescriptor(props), undefined, 2)
+    });
 })
 
 export default routes;
