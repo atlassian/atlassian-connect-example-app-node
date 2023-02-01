be8cdd7fefb7a2b9a5a9722f16f7353d9457de69
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-01 12:26:44 +1100

    - Minor styling added

---
+++ src/config/env.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { connectAppDescriptor } from '../config';
+import baseUrl from '../utils';
+
+const routes = Router();
+
+routes.get('/', (_req, res) => {
+    res.render('index.mst', {
+        index: 'Index Page',
+        body: 'You in the home page!'
+    });
+});
+
+routes.get('/config', async (req, res) => {
+    const props = { baseUrl: await baseUrl() };
+
+    if (req.query.isView) {
+        res.render('config.mst', {
+            index: 'Connect app descriptor',
+            config: JSON.stringify(connectAppDescriptor(props), undefined, 2)
+        });
+    } else {
+        res.json(connectAppDescriptor(props));
+    }
+})
+
+export default routes;
