352af6fff33be7458edcfe24abf49e94b2dfb947
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-03 15:20:58 +1100

    - webhooks logs added in DB file

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -1,22 +1,84 @@
 import { Router } from 'express';
+import { DBFile } from '../db';
 
 export const WebhooksRouter = Router();
 
+WebhooksRouter.get('/', (_, res) => {
+    DBFile.find({}, (error, data) => {
+            if (error) {
+                console.log("Error fetching logs: ", error);
+                res.status(500).json(error);
+            }else {
+                console.log("Logs fetched successfully", data);
+                res.json(data);
+            }
+        }
+    );
+});
+
 WebhooksRouter.post('/jira/issue-created', (req, res) => {
-    // TODO: add this log into DB
-    console.log('Jira Issue Created', req.body);
+    // Mapping the host set during the installation with the url coming in from the webhooks
+    const host = new URL(req.body.user.self).origin;
+
+    DBFile.update(
+        { host },
+        { $push: { logs: `Issue created ---> ${JSON.stringify(req.body)}` } },
+        {},
+        (error, data) => {
+            if (error) {
+                console.log("Error when adding Create logs: ", error);
+                res.status(500).json(error);
+            }else {
+                console.log("Create logs added successfully", data);
+                res.sendStatus(200);
+            }
+        }
+    );
+
     res.sendStatus(200);
 });
 
 WebhooksRouter.post('/jira/issue-updated', (req, res) => {
-    // TODO: add this log into DB
-    console.log('Jira Issue Updated', req.body);
+    // Mapping the host set during the installation with the url coming in from the webhooks
+    const host = new URL(req.body.user.self).origin;
+
+    DBFile.update(
+        { host },
+        { $push: { logs: `Issue updated ---> ${JSON.stringify(req.body)}` } },
+        {},
+        (error, data) => {
+            if (error) {
+                console.log("Error when adding Update logs: ", error);
+                res.status(500).json(error);
+            }else {
+                console.log("Update logs added successfully", data);
+                res.sendStatus(200);
+            }
+        }
+    );
+
     res.sendStatus(200);
 });
 
 WebhooksRouter.post('/jira/issue-deleted', (req, res) => {
-    // TODO: add this log into DB
-    console.log('Jira Issue Deleted', req.body);
+    // Mapping the host set during the installation with the url coming in from the webhooks
+    const host = new URL(req.body.user.self).origin;
+
+    DBFile.update(
+        { host },
+        { $push: { logs: `Issue deleted ---> ${JSON.stringify(req.body)}` } },
+        {},
+        (error, data) => {
+            if (error) {
+                console.log("Error when adding Delete logs: ", error);
+                res.status(500).json(error);
+            }else {
+                console.log("Delete logs added successfully", data);
+                res.sendStatus(200);
+            }
+        }
+    );
+
     res.sendStatus(200);
 });
 
