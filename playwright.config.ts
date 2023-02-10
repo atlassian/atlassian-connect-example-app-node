693bfde56e48c3c454555338c4b2c3139639a939
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-10 13:47:28 +1100

    - playwright installed

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: "./test/e2e",
+  testMatch: /.*\.e2e\.ts/,
+  outputDir: "./test/e2e/test-results",
+  use: {
+    trace: "retain-on-failure",
+    video: "retain-on-failure",
+    screenshot: "only-on-failure"
+  },
+  globalSetup: "./test/e2e/setup.ts",
+  globalTeardown: "./test/e2e/teardown.ts",
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    }
+  ]
+});
