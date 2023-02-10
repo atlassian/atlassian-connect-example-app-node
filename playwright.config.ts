072b2d9b9770668c8f92553a9b4c598e3be85d7a
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-02-10 14:54:06 +1100

    - Cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: "./test/e2e",
+  testDir: './test/e2e',
   testMatch: /.*\.e2e\.ts/,
-  outputDir: "./test/e2e/test-results",
+  outputDir: './test/e2e/test-results',
   timeout: 90 * 1000,
   use: {
-    trace: "retain-on-failure",
-    video: "retain-on-failure",
-    screenshot: "only-on-failure"
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure'
   },
-  globalSetup: "./test/e2e/setup.ts",
-  globalTeardown: "./test/e2e/teardown.ts",
+  globalSetup: './test/e2e/setup.ts',
+  globalTeardown: './test/e2e/teardown.ts',
 
   /* Configure projects for major browsers */
   projects: [
