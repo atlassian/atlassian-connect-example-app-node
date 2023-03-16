1729282e40b96489a7f3fa8e5590c3a31e4b63aa
Rachelle <rrathbone@atlassian.com>
2023-03-17 10:25:10 +1100

    wip

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,13 @@
 import path from "path";
 import { Router, static as Static } from "express";
+import { marked } from "marked";
 import { connectAppDescriptor, connectDescriptorGet } from "./atlassian-connect";
 import { eventsRouter } from "./events";
 import { logsRouter } from "./logs";
 import { webhooksRouter } from "./webhooks";
 import { connectIframeJWTMiddleware } from "../middlewares/connect-iframe-jwt-middleware";
+import * as fs from "fs";
+// import file from "../content/introduction";
 
 export const RootRouter = Router();
 
@@ -23,11 +26,16 @@ RootRouter.use("/webhooks", webhooksRouter);
 // Below are the Connect Module routes which need to pass the JWT check to continue
 RootRouter.use(connectIframeJWTMiddleware);
 
+// marked.setOptions({
+// 	renderer: new marked.Renderer()
+// });
 RootRouter.get("/", (_req, res) => {
+	const introductionPath = path.join(__dirname, '..', 'content', 'introduction.md')
+	const contents = fs.readFileSync(introductionPath);
+
 	res.render("index.mst", {
-		index: "Introduction",
-		body: "Welcome to the Node.js Sample App!"
-	});
+		content: marked(contents.toString())
+	})
 });
 
 RootRouter.get("/config", async (_req, res) => {
