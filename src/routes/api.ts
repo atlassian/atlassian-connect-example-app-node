39f642cddd9a2904bc2471cd39bfa5a738b6e8e9
Michel Boudreau <michelboudreau@gmail.com>
2023-04-04 20:20:58 +1000

    ARC-2059 Jira REST API example (#39)

---
+++ src/routes/api.ts
@@ -0,0 +1,37 @@
+import { Request, Response, Router } from "express";
+import { createQueryStringHash, encodeSymmetric } from "atlassian-jwt";
+import { envVars } from "../env";
+
+export const apiRouter = Router();
+
+apiRouter.get("/user/:userId", async (req: Request, res: Response) => {
+	// REST API url info
+	const pathname = "/rest/api/3/user";
+	const query = { accountId: req.params.userId };
+
+	// Creating JWT token for Jira REST API, using atlassian-jwt library: https://bitbucket.org/atlassian/atlassian-jwt-js/src/master/
+	const nowInSeconds = Math.floor(Date.now() / 1000);
+	const jwtToken = encodeSymmetric(
+		{
+			iat: nowInSeconds,
+			exp: nowInSeconds + 3 * 60, // 3 minutes
+			iss: envVars.APP_KEY,
+			qsh: createQueryStringHash({
+				method: "GET",
+				pathname,
+				query
+			})
+		},
+		// shared secret given as part of the installed lifecycle event
+		res.locals.jiraTenant.sharedSecret
+	);
+
+	// Call Jira REST API
+	const response = await fetch(`${envVars.ATLASSIAN_URL}${pathname}?${new URLSearchParams(query).toString()}`,{
+		headers: {
+			"Authorization": `JWT ${jwtToken}`
+		}
+	});
+
+	res.send(await response.json());
+});
---
+++ static/js/api.js
@@ -0,0 +1,10 @@
+function callAPI() {
+	var el = document.querySelector('#api-results');
+	el.innerHTML = "Loading...";
+	AP.user.getCurrentUser(function(user) {
+		AP.context.getToken(async function (token) {
+			var response = await fetch("/api/example/user/" + user.atlassianAccountId + "?jwt=" + token);
+			el.innerHTML = JSON.stringify(await response.json(), null, 2);
+		});
+	});
+}
