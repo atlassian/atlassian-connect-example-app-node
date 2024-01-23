e6a7dede1b529dfbe4f43826223835aa36c62e03
lcaglar <lcaglar@atlassian.com>
2024-01-23 14:38:46 +1100

    INCS-5658 migrate connected page

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -2,6 +2,7 @@ import { setGlobalTheme } from "@atlaskit/tokens";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartConnection from "./pages/StartConnection";
 import { startConnectionData } from "./pagesData/startConnectionData";
+import Connected from "./pages/Connected";
 
 const App = () => {
 	setGlobalTheme({
@@ -18,6 +19,7 @@ const App = () => {
 			<Routes>
 				<Route path="/spa">
 					<Route index element={<StartConnection {...startConnectionData} />} />
+					<Route path="connected" element={<Connected />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
---
+++ spa/src/pagesData/connectedData.tsx
@@ -0,0 +1,11 @@
+export interface startConnectionProps {
+	appName: string;
+	appLogoPath: string;
+	AppMarketplaceUrl: string;
+}
+
+export const startConnectionData = {
+	appName: "Your App",
+	appLogoPath: "/public/assets/jira-logo.svg", // Replace 'appLogoPath' with the path to your application's logo.
+	AppMarketplaceUrl: "",
+};
